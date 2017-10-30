aa704b7c43a588dc0f1b0080582d02d9183accfe
NinjMenon <ns229@duke.edu>
2017-10-30 19:49:14 -0400

    Functions to increment with view count and route to access count

--- ecg_api.py
+++ ecg_api.py
@@ -1,4 +1,19 @@
 from flask import Flask,jsonify, request
 import numpy as np 
 import math
+from multiprocessing import Value
 
+counter = Value('i', 0)
+app = Flask(__name__)
+
+
+@app.before_request()
+def before_request():
+    with counter.get_lock():
+        counter.value += 1
+        g.count = counter.value
+
+
+@app.route("/api/requests")
+def request_count():
+    return "Number of views {}".format(g.count)
