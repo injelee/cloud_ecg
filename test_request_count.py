593dab8aeb04d26ef4882ec623e5aab3bc4ba5fb
NinjMenon <ns229@duke.edu>
2017-10-30 19:48:05 -0400

    Storing view count in g object

--- test_request_count.py
+++ test_request_count.py
@@ -1,10 +1,10 @@
 from flask import Flask,request
 
 def test_response_count():
-    FLASK_APP=ecg_api.py flask run --host 0.0.0.0
+    FLASK_APP = ecg_api.py  flask  run  --host  0.0.0.0
     url = 'http://vcm-1849.vm.duke.edu:5000/'  #change to final server
     for i in range(10):
         request.get(url)
-    assert request_count == 10
+    assert g.count == 10
 
         
