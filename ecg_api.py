68449aa8db35d6fddc878f6b4afb914d86f301a4
NinjMenon <ns229@duke.edu>
2017-11-02 17:15:48 -0400

    Fixed some bugs in counting requests handled

--- ecg_api.py
+++ ecg_api.py
@@ -3,17 +3,13 @@ import numpy as np
 import math
 from multiprocessing import Value
 
-counter = Value('i', 0)
+counter = Value('i',0)
 app = Flask(__name__)
 
 
-@app.before_request()
-def before_request():
-    with counter.get_lock():
-        counter.value += 1
-        g.count = counter.value
-
-
 @app.route("/api/requests")
-def request_count():
-    return "Number of views {}".format(g.count)
+@app.before_request
+def inc_count():
+    counter.value += 1
+    count = counter.value
+    return jsonify("Number of views {}".format(count))
