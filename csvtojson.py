0f042b9be96402246462e81a0918549fb2ed0624
iel2 <iel2@duke.edu>
2017-11-03 15:45:43 -0400

    Modified csvtojson as module. Corrected input for dictionary usage in running script. Fixed testing inputs.

--- csvtojson.py
+++ csvtojson.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv(r'/Users/injelee/cloud_ecg/test_data/test_data9.csv', header = None)
-df.columns = ['time', 'voltage']
-df.time = df.time.astype(float)
-df.voltage = df.voltage.astype(float)
-t = df.time.tolist()
-v = df.voltage.tolist()
-data = {'time' : t, 'voltage' : v}
+
+def csvtojson():
+    df = pd.read_csv(r'/Users/injelee/cloud_ecg/test_data/test_data9.csv', header=None)
+    df.columns = ['time', 'voltage']
+    df.time = df.time.astype(float)
+    df.voltage = df.voltage.astype(float)
+    t = df.time.tolist()
+    v = df.voltage.tolist()
+    data = {'time': t, 'voltage': v}
+    return data
--- test_postavg.py
+++ test_postavg.py
@@ -4,40 +4,46 @@ Creating test class for average HR and brady/tachycardia data
 """
 
 import unittest
-from get_ecg import Ecg
+from bme590hrmfixed.get_ecg3 import Ecg
+from csvtojson import csvtojson
 
 
 class test_average_hr(unittest.TestCase):
 
     def test_output(self):
-        data = Ecg(csv_file='/Users/injelee/cloud_ecg/test_data/test_data9.csv', update_time=5,
-                   brady_threshold=60, tachy_threshold=100, user_sec=5)
-        data.prep_data()
-        data.get_max_peak()
-        data.get_inst_hr()
-        data.get_avghr()
-        data.as_dict()
-        assert isinstance(data.ecg_dict, dict) is True
+        data = csvtojson()
+        ecg_data = Ecg(data, update_time=5,
+                       brady_threshold=60, tachy_threshold=100, user_sec=5)
+        ecg_data.prep_data()
+        ecg_data.get_max_peak()
+        ecg_data.get_inst_hr()
+        ecg_data.get_avghr()
+        ecg_data.as_dict()
+        assert isinstance(ecg_data.ecg_dict, dict) is True
 
     def test_output_value(self):
-        data = Ecg(csv_file='/Users/injelee/cloud_ecg/test_data/test_data9.csv', update_time=5,
-                   brady_threshold=60, tachy_threshold=100, user_sec=5)
-        data.prep_data()
-        data.get_max_peak()
-        data.get_inst_hr()
-        data.get_avghr()
-        data.as_dict()
+        data = csvtojson()
+        ecg_data = Ecg(data, update_time=5,
+                       brady_threshold=60, tachy_threshold=100, user_sec=5)
+        ecg_data.prep_data()
+        ecg_data.get_max_peak()
+        ecg_data.get_inst_hr()
+        ecg_data.get_avghr()
+        ecg_data.as_dict()
         # create sample data for this case
-        assert data.avg_hr == [72.0, 60.673333333333332, 82.959999999999994, 93.36666666666666, 104.2]
+        assert ecg_data.avg_hr == [72.0, 60.673333333333332,
+                                   82.959999999999994, 93.36666666666666, 104.2]
 
     def test_output_lengths(self):
         # create sample data for this case
-        data = Ecg(csv_file='/Users/injelee/cloud_ecg/test_data/test_data9.csv', update_time=5,
-                   brady_threshold=60, tachy_threshold=100, user_sec=5)
-        data.prep_data()
-        data.get_max_peak()
-        data.get_inst_hr()
-        data.get_avghr()
-        data.as_dict()
-        assert len(data.brady) == len(data.tachy) == len(data.raw_bunches) == 10000
+        data = csvtojson()
+        ecg_data = Ecg(data, update_time=5,
+                       brady_threshold=60, tachy_threshold=100, user_sec=5)
+        ecg_data.prep_data()
+        ecg_data.get_max_peak()
+        ecg_data.get_inst_hr()
+        ecg_data.get_avghr()
+        ecg_data.as_dict()
+        assert len(ecg_data.brady) == len(ecg_data.tachy) == \
+            len(ecg_data.raw_bunches) == 10000
 
