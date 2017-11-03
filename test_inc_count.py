96267e32c0c3a3e6286e20ad0fd8388db481d55d
NinjMenon <ns229@duke.edu>
2017-11-03 16:48:34 -0400

    Attemtping to unit test request count

--- test_inc_count.py
+++ test_inc_count.py
@@ -1,12 +1,14 @@
 from flask import Flask,request
 from ecg_api import inc_count
+import flaskr
+import unittest
 
-
-def test_response_count():
-    url = 'http://vcm-1849.vm.duke.edu:5000/'  #change to final server
+class FlaskrTestCase(unittest.TestCase):
+    def test_response_count():
+    url = 'http://127.0.0.1:5000/api/'  #change to final server
     for i in range(10):
-        request.get(url)
-    c = inc_count()
+       self.app.get(url)
+    c =app.inc_count()
     assert c == 10
 
         
