3cefef4a17707a6db6f05fe48fe1ee7f8c4d5517
NinjMenon <ns229@duke.edu>
2017-11-03 16:56:07 -0400

    Flask testing

--- test_request_count.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,request
-
-def test_response_count():
-    url = 'http://vcm-1849.vm.duke.edu:5000/'  #change to final server
-    for i in range(10):
-        request.get(url)
-    assert g.count == 10
-
-        
