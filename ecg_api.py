3ca645a5ca728809ca41ac0913462cf85c36dae0
iel2 <iel2@duke.edu>
2017-11-03 18:47:08 -0400

    Added try/except for wrong inputs.

--- ecg_api.py
+++ ecg_api.py
@@ -3,9 +3,15 @@ import numpy as np
 import math
 from bme590hrmfixed.get_ecg3 import Ecg
 from csvtojson import csvtojson
-import csv
 
 app = Flask(__name__)
+
+
+def send_error(message, code):
+    err = {
+        "error": message
+    }
+    return jsonify(err), code
 
 
 @app.route("/api/heart_rate/average/", methods=["Post"])
@@ -17,11 +23,25 @@ def jsonavg():
 
     :return:
     """
+
     # Following two lines are for Postman testing with .csv input
     data1 = request.files['']
     data = csvtojson(data1)
     # data = request.json
+    try:
+        isinstance(data, dict) is True
+    except TypeError:
+        return send_error("Input was not of type=dictionary.", )
+
     avg_period = data["averaging_period"]
+
+    try:
+        isinstance(avg_period, float) is True
+    except TypeError:
+        return send_error("Averaging_period used could not be taken from the dictionary "
+                          "as type=float,"
+                          "or was not initially type float (e.g. str).", 400)
+
     ecg_data = Ecg(data, update_time=5, brady_threshold=60, tachy_threshold=100,
                    user_sec=avg_period)
     ecg_data.prep_data()
