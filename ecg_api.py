a7484bfa5a562052bc5e7868c0cd3f38e8ee7f7d
NinjMenon <ns229@duke.edu>
2017-11-03 20:24:20 -0400

    Added docstrings

--- ecg_api.py
+++ ecg_api.py
@@ -1,6 +1,4 @@
 from flask import Flask,jsonify, request
-import numpy as np 
-import math
 from multiprocessing import Value
 
 
@@ -11,6 +9,11 @@ app = Flask(__name__)
 @app.route("/api/requests")
 @app.before_request
 def inc_count():
+    """
+    Method that counts number of requests handled by the web service.
+    :params: None
+    :return: number of requests in json format
+    """
     counter.value += 1
     count = counter.value
-    return jsonify("Number of views {}".format(count))
+    return jsonify("Number of requests {}".format(count))
