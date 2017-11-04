e896c5ffe6aded1e84ba5103c3dff50831caada5
iel2 <iel2@duke.edu>
2017-11-03 20:35:17 -0400

    Updated tests with previous modifications with scripts.

--- test_postavg.py
+++ test_postavg.py
@@ -11,9 +11,10 @@ from csvtojson import csvtojson
 class test_average_hr(unittest.TestCase):
 
     def test_output(self):
-        data = csvtojson()
+        data = csvtojson('/Users/injelee/Desktop/test_data9_3.csv')
         ecg_data = Ecg(data, update_time=5,
-                       brady_threshold=60, tachy_threshold=100, user_sec=5)
+                       brady_threshold=60, tachy_threshold=100,
+                       user_sec=5, status=1)
         ecg_data.prep_data()
         ecg_data.get_max_peak()
         ecg_data.get_inst_hr()
@@ -22,9 +23,10 @@ class test_average_hr(unittest.TestCase):
         assert isinstance(ecg_data.ecg_dict, dict) is True
 
     def test_output_value(self):
-        data = csvtojson()
+        data = csvtojson('/Users/injelee/Desktop/test_data9_3.csv')
         ecg_data = Ecg(data, update_time=5,
-                       brady_threshold=60, tachy_threshold=100, user_sec=5)
+                       brady_threshold=60, tachy_threshold=100,
+                       user_sec=5, status=1)
         ecg_data.prep_data()
         ecg_data.get_max_peak()
         ecg_data.get_inst_hr()
@@ -32,18 +34,20 @@ class test_average_hr(unittest.TestCase):
         ecg_data.as_dict()
         # create sample data for this case
         assert ecg_data.avg_hr == [72.0, 60.673333333333332,
-                                   82.959999999999994, 93.36666666666666, 104.2]
+                                   82.959999999999994,
+                                   93.36666666666666, 104.2]
 
     def test_output_lengths(self):
         # create sample data for this case
-        data = csvtojson()
+        data = csvtojson('/Users/injelee/Desktop/test_data9_3.csv')
         ecg_data = Ecg(data, update_time=5,
-                       brady_threshold=60, tachy_threshold=100, user_sec=5)
+                       brady_threshold=60, tachy_threshold=100,
+                       user_sec=5, status=1)
         ecg_data.prep_data()
         ecg_data.get_max_peak()
         ecg_data.get_inst_hr()
         ecg_data.get_avghr()
+        ecg_data.get_bradtach()
         ecg_data.as_dict()
-        assert len(ecg_data.brady) == len(ecg_data.tachy) == \
-            len(ecg_data.raw_bunches) == 10000
+        assert len(ecg_data.brady) == len(ecg_data.tachy) == 5
 
