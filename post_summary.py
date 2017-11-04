0e278a58032c80f34e3bc4849b4af6debd27d72c
jl3392 <jingli3392@gmail.com>
2017-11-03 20:11:49 -0400

    try the raw data, update the data input.

--- post_summary.py
+++ post_summary.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from bme590hrmfixed.get_ecg3 import Ecg
 import json
+from csvtojson import csvtojson
 app = Flask(__name__)
 
 
@@ -11,10 +12,11 @@ def send_error(message, code):
     return jsonify(err), code
 
 
-@app.route("api/heart_rate/summary", methods=['POST'])
+@app.route("/api/heart_rate/summary", methods=['POST'])
 def summary():
-    if request.method == 'POST':
-        data = request.json
+    # data = request.json
+    data_raw = request.files['']
+    data = csvtojson(data_raw)
 
     try:
         isinstance(data, dict) is True
@@ -22,17 +24,19 @@ def summary():
         return send_error("The input is not in dict format", 400)  # 400 refers to bad request
 
     try:
-        len(data['time']) != 0 and len(data['voltage']) != 0
+        len(data["time"]) > 0 and len(data["voltage"]) > 0
     except ValueError:
         return send_error("The input is empty ", 400)  # 400 refers to bad request
 
     data = Ecg(data, update_time=5,
-               brady_threshold=60, tachy_threshold=100, mins=2)
+               brady_threshold=60, tachy_threshold=100, user_sec=10, status=0)
     data.prep_data()
     data.get_max_peak()
     data.get_inst_hr()
     data.get_avghr()
-    return_summary = data.ecg_summary()
+    data.get_bradtach()
+    data.summary()
+    return_summary = data.ecg_summary
     return jsonify(return_summary)
 
 
